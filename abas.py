ea34db8e76c97822f9a863376909fc199832fafe
Ivan Sousa <66036659+ivanjsousa@users.noreply.github.com>
2022-11-17 21:55:27 -0300

    Atualização
    Código alterado para possibilitar impressão do formulario

--- abas.py
+++ abas.py
@@ -1,28 +1,36 @@
 import PySimpleGUI as Sg
 
+
 # ------ LAYOUT DAS ABAS - (TIPO PASTAS) ------ #
 
 tab1_layout = [[Sg.T('Quantas cópias do produto?')],
-               [Sg.In(key='in', do_not_clear=False, size=(55, 5))]]
+               [Sg.In(key='quantidade', do_not_clear=False, size=(55, 5))]]
 
 tab2_layout = [[Sg.T('Como pensa do Formato Aberto e Fechado?')],
-               [Sg.In(key='in', do_not_clear=False, size=(55, 5))]]
+               [Sg.In(key='formato', do_not_clear=False, size=(55, 5))]]
 
 tab3_layout = [[Sg.T('Qual vai ser o Revestimento do Livro?')],
-               [Sg.In(key='in', do_not_clear=False, size=(55, 5))]]
+               [Sg.In(key='revest', do_not_clear=False, size=(55, 5))]]
 
 tab4_layout = [[Sg.T('Como será a folha de Guarda?')],
-               [Sg.In(key='in', do_not_clear=False, size=(55, 5))]]
+               [Sg.In(key='guarda', do_not_clear=False, size=(55, 5))]]
 
 tab5_layout = [[Sg.T('Descreva o Papel do Miolo?')],
-               [Sg.In(key='in', do_not_clear=False, size=(55, 5))]]
+               [Sg.In(key='miolo', do_not_clear=False, size=(55, 5))]]
 
-tab6_layout = [[Sg.T('Quantos cadernos e/ou páginas?')],
-               [Sg.In(key='in', do_not_clear=False, size=(55, 5))]]
+tab6_layout = [[Sg.T('Serão quantas páginas?')],
+               [Sg.In(key='caderno', do_not_clear=False, size=(55, 5))]]
 
-abas = [Sg.TabGroup([[Sg.Tab('Quantidade', tab1_layout, key='qualidade'),
-                      Sg.Tab('Formato', tab2_layout, key='formato'),
-                      Sg.Tab('Revestimento', tab3_layout, key='revest'),
-                      Sg.Tab('Guarda', tab4_layout, key='guarda'),
-                      Sg.Tab('Miolo', tab5_layout, key='miolo'),
-                      Sg.Tab('Cadernos', tab6_layout, key='caderno')]])],
+abas = [Sg.TabGroup([[Sg.Tab('Quantidade', tab1_layout),
+                      Sg.Tab('Formato', tab2_layout),
+                      Sg.Tab('Revestimento', tab3_layout),
+                      Sg.Tab('Guarda', tab4_layout),
+                      Sg.Tab('Miolo', tab5_layout),
+                      Sg.Tab('Páginas', tab6_layout)]])],
+
+quantidade = 'quantidade'
+formato = 'formato'
+revestimeno = 'revest'
+guarda = 'guarda'
+miolo = 'miolo'
+paginas = 'paginas'
