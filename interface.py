e39b0a175c1d41410e875cc77742703c2cd8d85f
Ivan Sousa <66036659+ivanjsousa@users.noreply.github.com>
2022-11-17 21:50:42 -0300

    Atualização da Interface
    Resolvido o problema de imprimir a log na tela, agora é possível ver o pedido com o texto formatado com os itens escolhidos no formulário

--- interface.py
+++ interface.py
@@ -21,7 +21,7 @@ layout = [
     [fm.caract_projeto],
     [fm.papelao_capa],
     [ab.abas],
-    [Sg.Output(size=(55, 4))],
+    [Sg.Output(size=(55, 5))],
 
     [Sg.Button('Novo'), Sg.SaveAs('Salvar'), Sg.Button('Imprimir'), Sg.Button('Sair')]
 ]
@@ -41,7 +41,7 @@ while True:
     if sair == 'Yes':
         break
 
-    if evento == 'Novo' or evento == Sg.Button('Novo'):
+    if evento == 'Novo':
         do_not_clear = True
 
     if evento == 'Imprimir':
@@ -49,6 +49,21 @@ while True:
         print(f'Cliente: ', valor['cliente'])
         print(f'Telefone: ', valor['tel'])
         print(f'E-mail: ', valor['email'])
+        for tipo in fm.Lista_Tipo:
+            if valor[tipo] is True:
+                print(f'Tipo: {tipo}.')
+        for item in fm.Lista_Acabamento:
+            if valor[item] is True:
+                print(f'Com: {item}.')
+        for forma in fm.Lista_Impressao:
+            if valor[forma] is True:
+                print(f'Impressão: {forma}.')
+        for capa in fm.Lista_Papelao:
+            if valor[capa] is True:
+                print(f'Papelão da Capa de: {capa}.')
+        print(f'O projeto terá {valor[ab.quantidade]} unidades(s) no formato {valor[ab.formato]}, revestimento '
+              f'em {valor[ab.revestimeno]} e papel {valor[ab.guarda]} para a guarda, o'
+              f'miolo com papel {valor[ab.miolo]} e serão {valor[ab.paginas]} páginas.')
 
 
 # ------ FECHAR A JANELA ------- #
