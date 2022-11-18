a793edfb04f1f8fa11aa1a6ccc9edfaa923cc310
Ivan Sousa <ivanjsousa@gmail.com>
2022-11-18 13:25:33 -0300

    Implementando o autenticador dentro de um 'if' que se a variável 'status' dentro do módulo login for True o sistema continua e abre, do contrario ele não executa

--- interface.py
+++ interface.py
@@ -4,10 +4,6 @@ import input_users as iu
 import abas as ab
 import login as lg
 
-# ------ AUTENTICAÇÃO USER\SENHA ------ #
-
-autenticar = [lg.login()]
-
 # ------ BARRA DE MENU ------ #
 
 menu_def = [['&Arquivo', ['&Novo', '---', '&Salvar', '&Imprimir', 'Sai&r']],
@@ -25,46 +21,49 @@ layout = [
     [Sg.Button('Novo'), Sg.SaveAs('Salvar'), Sg.Button('Imprimir'), Sg.Button('Sair')]
 ]
 
-# ------ CHAMA A JANELA (INTERFACE GRÁFICA) ------ #
 
-janela = Sg.Window("Projetos de Encadernação V1.0", layout, enable_close_attempted_event=True)
+# ------ AUTENTICAÇÃO USER\SENHA ------ #
 
+autenticar = [lg.login()]
+if lg.status is True:
 
-# ------ LAÇO QUE MANTEM A JANELA ATIVA ------ #
-""" Aqui é colocado toda a lógica de funcionamento dos eventos e valores do aplicativo"""
+    # ------ CHAMA A JANELA DO PROGRAMA ------- #
+    janela = Sg.Window("Projetos de Encadernação V1.0", layout, enable_close_attempted_event=True)
 
-while True:
-    evento, valor = janela.read()
-    sair = (evento == Sg.WIN_CLOSE_ATTEMPTED_EVENT or evento == 'Sair') and Sg.popup_yes_no('Deseja sair?')
+    # ------ LAÇO QUE MANTEM A JANELA ATIVA ------ #
+    """ Aqui é colocado toda a lógica de funcionamento dos eventos e valores do aplicativo"""
 
-    if sair == 'Yes':
-        break
+    while True:
+        evento, valor = janela.read()
+        sair = (evento == Sg.WIN_CLOSE_ATTEMPTED_EVENT or evento == 'Sair') and Sg.popup_yes_no('Deseja sair?')
 
-    if evento == 'Novo':
-        do_not_clear = True
+        if sair == 'Yes':
+            break
 
-    if evento == 'Imprimir':
-        print(f'Projeto: ', valor['projeto'])
-        print(f'Cliente: ', valor['cliente'])
-        print(f'Telefone: ', valor['tel'])
-        print(f'E-mail: ', valor['email'])
-        for tipo in fm.Lista_Tipo:
-            if valor[tipo] is True:
-                print(f'Tipo: {tipo}.')
-        for item in fm.Lista_Acabamento:
-            if valor[item] is True:
-                print(f'Com: {item}.')
-        for forma in fm.Lista_Impressao:
-            if valor[forma] is True:
-                print(f'Impressão: {forma}.')
-        for capa in fm.Lista_Papelao:
-            if valor[capa] is True:
-                print(f'Papelão da Capa de: {capa}.')
-        print(f'O projeto terá {valor[ab.quantidade]} unidades(s) no formato {valor[ab.formato]}, revestimento '
-              f'em {valor[ab.revestimeno]} e papel {valor[ab.guarda]} para a guarda, o'
-              f'miolo com papel {valor[ab.miolo]} e serão {valor[ab.paginas]} páginas.')
+        if evento == 'Novo':
+            do_not_clear = True
 
+        if evento == 'Imprimir':
+            print(f'Projeto: ', valor['projeto'])
+            print(f'Cliente: ', valor['cliente'])
+            print(f'Telefone: ', valor['tel'])
+            print(f'E-mail: ', valor['email'])
+            for tipo in fm.Lista_Tipo:
+                if valor[tipo] is True:
+                    print(f'Tipo: {tipo}.')
+            for item in fm.Lista_Acabamento:
+                if valor[item] is True:
+                    print(f'Com: {item}.')
+            for forma in fm.Lista_Impressao:
+                if valor[forma] is True:
+                    print(f'Impressão: {forma}.')
+            for capa in fm.Lista_Papelao:
+                if valor[capa] is True:
+                    print(f'Papelão da Capa de: {capa}.')
+            print(f'O projeto terá {valor[ab.quantidade]} unidades(s) no formato {valor[ab.formato]}, revestimento '
+                  f'em {valor[ab.revestimeno]} e papel {valor[ab.guarda]} para a guarda, o'
+                  f' miolo com papel {valor[ab.miolo]} e serão {valor[ab.paginas]} páginas.')
 
-# ------ FECHAR A JANELA ------- #
+    # ------ FECHAR A JANELA ------- #
 
-janela.close()
+    janela.close()
--- login.py
+++ login.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as Sg
 
+status = False
+
 
 def login():
     layout = [
@@ -7,7 +9,7 @@ def login():
         [Sg.InputText(key='inpt_user')],
         [Sg.Text('Senha', key='senha')],
         [Sg.InputText(key='inpt_senha', password_char='*')],
-        [Sg.Button('Login'), Sg.Button('Cancelar')],
+        [Sg.Button('Login')],
         ]
 
     # ------ CHAMAR JANELA ------ #
@@ -20,14 +22,14 @@ def login():
         evento, valor = janela.read()
         if evento == Sg.WIN_CLOSED:
             break
-        elif evento == 'Cancelar':
-            break
         elif evento == 'Login':
             usuario_correto = 'teste'
             senha_correta = '123'
             user = valor['inpt_user']
             senha = valor['inpt_senha']
             if user == usuario_correto and senha == senha_correta:
+                global status
+                status = True
                 Sg.popup_ok('Login Autenticado com sucesso!', no_titlebar=True)
                 janela.close()
             else:
